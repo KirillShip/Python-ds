f753daa16061858621b2310eff6c9da2d0b8217b
Kirill <shkirkala@gmail.com>
2023-10-20 19:28:42 +0500

    changed logic

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
 import telebot
 from telebot import types
+import re
 
 TOKEN = '6447996107:AAFMcuJPDWTOi2zUFdA_hNOpUGh-dV5a4wQ'
 
 bot = telebot.TeleBot(TOKEN)
 
-stage = 0
+phone_number_regex = re.compile(r'^(\+7|8)\d{10}$')
+name_regex = re.compile(r'^[А-ЯЁ][а-яё]+\s[А-ЯЁ][а-яё]+(\s[А-ЯЁ][а-яё]+)?$')
+name = ''
+phone_number = ''
 
-@bot.message_handler(commands=['startquest'])
-def start_quest(message):
-  markup = types.InlineKeyboardMarkup()
-  itembtn1 = types.InlineKeyboardButton(text='Преодолевать желание зайти в лес и попытаться обойти его', callback_data='Вариант 1')
-  itembtn2 = types.InlineKeyboardButton(text='Пойти вглубь леса', callback_data='Вариант 2')
-  markup.add(itembtn1, itembtn2)
-  bot.send_message(message.chat.id, "Вы просыпаетесь на окраине леса и не понимаете как здесь оказались. Вы думаете, что нужно как-то вернуться домой, но что-то притягивает вас вглубь зарослей.", reply_markup=markup)
-
-def quest_stage1(message, variant):
-  pass
-
-def quest_stage2(message, variant):
-  pass
-
-def quest_stage3(message, variant):
-  pass
-
-def quest_stage4(message, variant):
-  pass
-
-def quest_stage5(message, variant):
-  pass
-
-@bot.callback_query_handler(func=lambda call: True)
-def answering(call):
-  if stage == 0:
-    quest_stage1(call.message, call.data)
-  elif stage == 1:
-    quest_stage2(call.message, call.data)
-  elif stage == 2:
-    quest_stage3(call.message, call.data)
-  elif stage == 3:
-    quest_stage4(call.message, call.data)
-  elif stage == 4:
-    quest_stage5(call.message, call.data)
-
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Пожалуйста, введите свой номер телефона и ФИО в разных сообщениях для записи")
     
+# Обработчик текстовых сообщений
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    global name
+    global phone_number
+    if message.text.startswith('/start'):
+        return
+    if message.text.startswith('/getID'):
+        bot.send_message(message.chat.id, message.chat.id)
+        return
+    if message.text.startswith('/'):
+        bot.send_message(message.chat.id, 'Неверная команда')
+        return
+    if phone_number_regex.match(message.text):
+        bot.send_message(message.chat.id, 'Вы успешно ввели номер телефона')
+        phone_number = message.text
+        if (name != '' and phone_number != ''):
+            bot.send_message(-4023403322, f'Имя: {name}\nТелефон: {phone_number}')
+            bot.send_message(message.chat.id, 'Вы успешно записались на пробное занятие. С вами свяжется наш менеджер')
+        return
+    if name_regex.match(message.text):
+        bot.send_message(message.chat.id, 'Вы успешно ввели Ваше ФИО')
+        name = message.text
+        if (name != '' and phone_number != ''):
+            bot.send_message(-4023403322, f'Имя: {name}\nТелефон: {phone_number}')
+            bot.send_message(message.chat.id, 'Вы успешно записались на пробное занятие. С вами свяжется наш менеджер')
+        return
+    else:
+        bot.send_message(message.chat.id, 'Повторите попытку')
+        return
+
 bot.infinity_polling()
