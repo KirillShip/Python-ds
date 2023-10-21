12192736b2cd86d6fe7342c238fe6afb6a1fae1f
Kirill <shkirkala@gmail.com>
2023-10-21 17:22:59 +0500

    new version

--- main.py
+++ main.py
@@ -7,19 +7,38 @@ TOKEN = '6447996107:AAFMcuJPDWTOi2zUFdA_hNOpUGh-dV5a4wQ'
 bot = telebot.TeleBot(TOKEN)
 
 phone_number_regex = re.compile(r'^(\+7|8)\d{10}$')
-name_regex = re.compile(r'^[А-ЯЁ][а-яё]+\s[А-ЯЁ][а-яё]+(\s[А-ЯЁ][а-яё]+)?$')
-name = ''
-phone_number = ''
+age_regex = re.compile(r'^\d.*')
+district_regex = re.compile(r'^\D.*')
+data = {}
+request_chat_id = '-4023403322'
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Пожалуйста, введите свой номер телефона и ФИО в разных сообщениях для записи")
+def enter_district(message):
+    clear_data(message)
+    data[message.chat.id] = {'stage':0}
+    markup = types.InlineKeyboardMarkup()
+    itembtn1 = types.InlineKeyboardButton(text='Центр', callback_data='Центр')
+    itembtn2 = types.InlineKeyboardButton(text='ВИЗ', callback_data='ВИЗ')
+    itembtn3 = types.InlineKeyboardButton(text='Уралмаш', callback_data='Уралмаш')
+    itembtn4 = types.InlineKeyboardButton(text='Академический', callback_data='Академический')
+    itembtn5 = types.InlineKeyboardButton(text='ЖБИ', callback_data='ЖБИ')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5)
+    bot.send_message(message.chat.id, 'Пожалуйста, выберите Ваш район ',reply_markup=markup)
+
+def enter_age(message):
+    markup = types.InlineKeyboardMarkup()
+    itembtn1 = types.InlineKeyboardButton(text='6-8 лет', callback_data='6-8')
+    itembtn2 = types.InlineKeyboardButton(text='9-11 лет', callback_data='9-11')
+    itembtn3 = types.InlineKeyboardButton(text='12-14 лет', callback_data='12-14')
+    markup.add(itembtn1, itembtn2, itembtn3)
+    bot.send_message(message.chat.id, 'Пожалуйста, укажите возраст вашего ребенка',reply_markup=markup)
+
+
+def enter_phone_number(message):
+    bot.send_message(message.chat.id, 'Пожалуйста, введите номер телефона, по которому мы можем с Вами связаться')
     
-# Обработчик текстовых сообщений
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    global name
-    global phone_number
     if message.text.startswith('/start'):
         return
     if message.text.startswith('/getID'):
@@ -28,22 +47,37 @@ def handle_text(message):
     if message.text.startswith('/'):
         bot.send_message(message.chat.id, 'Неверная команда')
         return
-    if phone_number_regex.match(message.text):
-        bot.send_message(message.chat.id, 'Вы успешно ввели номер телефона')
-        phone_number = message.text
-        if (name != '' and phone_number != ''):
-            bot.send_message(-4023403322, f'Имя: {name}\nТелефон: {phone_number}')
-            bot.send_message(message.chat.id, 'Вы успешно записались на пробное занятие. С вами свяжется наш менеджер')
-        return
-    if name_regex.match(message.text):
-        bot.send_message(message.chat.id, 'Вы успешно ввели Ваше ФИО')
-        name = message.text
-        if (name != '' and phone_number != ''):
-            bot.send_message(-4023403322, f'Имя: {name}\nТелефон: {phone_number}')
-            bot.send_message(message.chat.id, 'Вы успешно записались на пробное занятие. С вами свяжется наш менеджер')
+    if phone_number_regex.match(message.text) and data[message.chat.id]['stage'] == 2:
+        data[message.chat.id]['phone_number'] = message.text
+        data[message.chat.id]['stage'] = 3
+        check_and_send(message)
         return
     else:
         bot.send_message(message.chat.id, 'Повторите попытку')
         return
 
+def check_and_send(message):
+    if district_regex.match(data[message.chat.id]['district']) and age_regex.match(data[message.chat.id]['age']):
+        bot.send_message(message.chat.id, 'Спасибо! Скоро с вами свяжется администратор!')
+        bot.send_message(request_chat_id, 'Район ' + data[message.chat.id]['district']+' возраст '+data[message.chat.id]['age']+' '+data[message.chat.id]['phone_number'])
+        clear_data(message)
+    else:
+        bot.send_message(message.chat.id, 'Неправильно сформированы ответы на вопросы, поробуйте еще раз')
+        enter_district(message)
+    
+def clear_data(message):
+    if message.chat.id in data:
+        del data[message.chat.id]
+  
+@bot.callback_query_handler(func=lambda call: True)
+def answering(call):
+    if call.message.chat.id in data:
+        if data[call.message.chat.id]['stage'] == 0:
+            data[call.message.chat.id]['district'] = call.data
+            data[call.message.chat.id]['stage'] = 1
+            enter_age(call.message)
+        elif data[call.message.chat.id]['stage'] == 1:
+            data[call.message.chat.id]['age'] = call.data
+            data[call.message.chat.id]['stage'] = 2
+            enter_phone_number(call.message)
 bot.infinity_polling()
